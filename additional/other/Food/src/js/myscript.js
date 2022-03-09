97570a785d9aae3690126be48fc7f7f347e569d7
Aleks Pashkevych <alekspashkevych@gmail.com>
2022-03-09 21:16:20 +0200

    added tabs, chanhed output script to myscript.js and made some script changes to gulp. All works

---
+++ additional/other/Food/src/js/myscript.js
@@ -0,0 +1,47 @@
+/*jshint esversion: 9 */
+/* jslint node: true */
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+   console.log("web page started");
+
+   const tabs = document.querySelectorAll('.tabheader__item');
+   const tabContent = document.querySelectorAll('.tabcontent');
+   const tabParent = document.querySelector('.tabcontainer');
+
+   function hideTabContent() {
+      tabContent.forEach(item => {
+         item.classList.add("hide");
+         item.classList.remove("show","fade");
+      });
+
+      tabs.forEach(item => {
+         item.classList.remove('tabheader__item_active');
+      });
+   }
+
+   function showTabContent(currentTab = 0) {
+      tabContent[currentTab].classList.add("show", "fade");
+      tabContent[currentTab].classList.remove("hide");
+      tabs[currentTab].classList.add('tabheader__item_active');
+   }
+
+   hideTabContent();
+   showTabContent();
+
+   tabParent.addEventListener('click', (event) =>{
+      event.preventDefault();
+
+      const target = event.target;
+      if (target && target.classList.contains('tabheader__item')) {
+         hideTabContent();
+         tabs.forEach((item,i) => {
+            if (item == target){
+               hideTabContent();
+               showTabContent(i);
+               
+            }
+         });
+      }
+   });
+});
