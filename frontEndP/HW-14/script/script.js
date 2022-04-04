d52521f9a938511406e39ecfafb9026cdfa5bbed
Aleks Pashkevych <alekspashkevych@gmail.com>
2022-04-04 19:21:52 +0300

    HW-14 version 1, removed ocasionaly made error

--- frontEndP/HW-14/script/script.js
+++ frontEndP/HW-14/script/script.js
@@ -7,10 +7,10 @@ class TodoApp {
     this.todoList = [];
   }
   add(item) {
-    if (item.isPinned == true) {
+    if (item.isPinned == true){
       this.todoList.unshift(item);
-    } else {
-      this.todoList.push(item);
+    }else{
+    this.todoList.push(item);
     }
   }
   remove(id) {
@@ -35,85 +35,7 @@ class TodoItem {
     this.createdDate = new Date();
   }
   updateName(name) {
-    this.name = name;/*jshint esversion: 9 */
-    /* jslint node: true */
-    "use strict";
-    
-    class TodoApp {
-      constructor() {
-        this.todoList = [];
-      }
-      add(item) {
-        if (item.isPinned == true){
-          this.todoList.unshift(item);
-        }else{
-        this.todoList.push(item);
-        }
-      }
-      remove(id) {
-        for (let i = 0; i < this.todoList.length; i++) {
-          if (this.todoList[i].id == id) {
-            this.todoList.splice(this.todoList[i], 1);
-          }
-        }
-      }
-      clear() {
-        this.todoList = [];
-      }
-    }
-    class TodoItem {
-      constructor({
-        name,
-        id,
-        createdDate
-      }) {
-        this.name = name;
-        this.id = id;
-        this.createdDate = new Date();
-      }
-      updateName(name) {
-        this.name = name;
-      }
-      generateId() {
-        this.id = Math.floor(Math.random() * 10000);
-      }
-    }
-    class PinnedTodoItem extends TodoItem {
-      isPinned = true;
-      
-    }
-    class ExpireTodoItem extends TodoItem {
-      canExpire = true;
-      constructor(obj, delay) {
-        this.obj = obj;
-        this.delay = delay;
-        const now = new Date();
-        this.expireTimeStamp = now.getTime() + 10000;
-      }
-      check(){
-        if (new Date().getTime() >= this.expireTimeStamp){
-          this.remove(this.obj.id);
-        }
-      }
-    }
-    
-    const myApp = new TodoApp();
-    const newItem = new TodoItem("wash my hands");
-    newItem.updateName("To do my homework");
-    newItem.generateId();
-    const otherItem = new TodoItem({
-      name: "brush my teeth",
-      id: null,
-      createdDate: null,
-    });
-    otherItem.generateId();
-    myApp.add(newItem);
-    myApp.add(otherItem);
-    console.log(myApp);
-    myApp.remove(newItem.id);
-    console.log(myApp);
-    
-    
+    this.name = name;
   }
   generateId() {
     this.id = Math.floor(Math.random() * 10000);
@@ -121,7 +43,7 @@ class TodoItem {
 }
 class PinnedTodoItem extends TodoItem {
   isPinned = true;
-
+  
 }
 class ExpireTodoItem extends TodoItem {
   canExpire = true;
@@ -131,8 +53,8 @@ class ExpireTodoItem extends TodoItem {
     const now = new Date();
     this.expireTimeStamp = now.getTime() + 10000;
   }
-  check() {
-    if (new Date().getTime() >= this.expireTimeStamp) {
+  check(){
+    if (new Date().getTime() >= this.expireTimeStamp){
       this.remove(this.obj.id);
     }
   }
@@ -153,3 +75,4 @@ myApp.add(otherItem);
 console.log(myApp);
 myApp.remove(newItem.id);
 console.log(myApp);
+
