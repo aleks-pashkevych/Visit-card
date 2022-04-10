422d8e8aefa9f13da35f66d49bfd92b75e8bb7df
Aleks Pashkevych <alekspashkevych@gmail.com>
2022-04-10 15:00:28 +0300

    Task 2 version-1 looks working

--- frontEndP/HW-12/script/script.js
+++ frontEndP/HW-12/script/script.js
@@ -20,3 +20,39 @@ alert(str);
 str = makeString('Test')('super')('test');
 console.log(str);
 alert(str);
+
+
+// & TASK 2
+
+const person = {
+   name: "Alex",
+   dob: 1995,
+   makeRequest() {
+    alert("Request sended");
+   },   
+  };
+
+  const otherPerson = {
+   name: "Evgen",
+   dob: 2012,
+   makeRequest() {
+    alert("Request sended");
+   },   
+  };
+  
+  const log = [];
+  function guard(func, prs){
+   const cYear = new Date().currentYear;
+   const age = cYear - prs.dob;
+   func;
+   let rez = false;
+   if(age >= 18) {
+      rez = true;
+      alert("Access granted.");
+   }   
+log.push({time: new Date(), user: prs.name, Access: rez});
+}
+
+guard(person.makeRequest(), person);
+guard(otherPerson.makeRequest(), otherPerson);
+console.log(log);
