0a250c3a6f89021664a28592b4d002040aa38d30
Aleksandr Pashkevych <alekspashkevych@gmail.com>
2022-04-29 16:23:05 +0300

    Finished default GULP-PROJ, git add -Awithout FTP works and initiated GULP-FISH-Proj, ready for deploy

---
+++ gulp/src/script/main.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from "./modules/function.js";
+
+flsFunctions.isWebp();
+
+
+import Swiper, {
+   Navigation,
+   Pagination
+} from "swiper";
+
+const swiper = new Swiper();
