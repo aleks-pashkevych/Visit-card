128ada8ed62c698165c857497f3aba2e3bce00af
Aleksandr Pashkevych <alekspashkevych@gmail.com>
2022-05-13 19:20:13 +0300

    feat: implemented modal window, ready to send

--- frontEndP/HW-17/src/js/main.js
+++ frontEndP/HW-17/src/js/main.js
@@ -18,3 +18,23 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
    },
 });
+
+
+//Modal
+
+const modalTrigger = document.querySelectorAll('.modal-btn');
+const modalWindow = document.querySelector('.modal');
+
+modalTrigger.forEach(element => {
+   element.addEventListener('click', (event) => {
+      modalWindow.classList.add('show');
+      modalWindow.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+   });
+});
+
+modalWindow.addEventListener('click', (event) => {
+   modalWindow.classList.add('hide');
+   modalWindow.classList.remove('show');
+   document.body.style.overflow = '';
+});
